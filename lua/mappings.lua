2c0b03e43c92be850e3cc67bd27caa99be705ebb
fede183 <fede.183@hotmail.com>
2023-06-04 13:36:37 -0300

    fonts added

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-vim.keymap.set("n", "<leader>s", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>s", builtin.grep_string, {})
 
 local api = require("nvim-tree.api")
 vim.keymap.set("n", "<leader>e", api.tree.toggle, {})
