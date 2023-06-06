de25ecf089a8699002e97d921ed11baf1af1f6f1
fede183 <fede.183@hotmail.com>
2023-06-06 08:50:55 -0300

    requirements script added

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,10 @@
 vim.g.mapleader = " "
 
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-vim.keymap.set("n", "<leader>s", builtin.grep_string, {})
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 local api = require("nvim-tree.api")
 vim.keymap.set("n", "<leader>e", api.tree.toggle, {})
---
+++ requirements.sh
@@ -0,0 +1,3 @@
+apt install ripgrep;
+snap install nvim;
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim;
