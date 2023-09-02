e65c1f8b3b309dab1864e9b80d8246746a1a29b2
fede183 <fede.183@hotmail.com>
2023-09-02 13:56:42 -0300

    groff basic command

--- nvim-exclusive/init.lua
+++ nvim-exclusive/init.lua
@@ -3,11 +3,18 @@ require('mappings')
 require('treesitter-config')
 require('nvim-tree-config')
 require('lsp-config')
-require('scripts.lua')
+require('scripts')
 
-local set = vim.opt
-local cmd = vim.cmd
+vim.opt.number = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
-set.number = true
+vim.cmd('syntax on')
 
-cmd('syntax on')
+vim.cmd('colorscheme ron')
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'list:longest'
+vim.opt.wildignore = '*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx'
+
+
--- nvim-exclusive/lua/scripts.lua
+++ nvim-exclusive/lua/scripts.lua
@@ -1,7 +1,15 @@
-local api = vim.api
+function GroffCompile()
+	local file = vim.fn.expand('%:f')
+	local filename = vim.fn.expand('%:r')
+	local filetype = vim.fn.expand('%:e')
 
-local filepath = vim.fn.expand('%:f')
-local groff_command = string.format('! groff -ms %s -T pdf > %s.pdf', filepath, filepath)
+	if filetype ~= 'ms' then
+		vim.api.nvim_err_writeln('the file type is invalid, should be .ms')
+		return
+	end
+	
+	local groff_command = string.format('! groff -ms %s -T pdf > %s.pdf', file, filename)
+	vim.cmd(groff_command)
+end
 
-api.nvim_create_user_command('GroffCompile', groff_command, {})
--- cmd.GroffProgram()
+vim.api.nvim_create_user_command('GroffCompile', GroffCompile, {})
