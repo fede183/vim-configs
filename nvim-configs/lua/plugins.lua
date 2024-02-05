97e06e3b3cae44128f036f5bcb420e5cf6e5e2e9
fede183 <fede.183@hotmail.com>
2024-02-05 10:02:05 -0300

    lazy nvim

--- nvim-configs/lua/plugins.lua
+++ nvim-configs/lua/plugins.lua
@@ -1,51 +1,44 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+require("lazy").setup({
 
-  use { 'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { { 'nvim-lua/plenary.nvim' } } }
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  'nvim-treesitter/nvim-treesitter',
 
-  use 'nvim-tree/nvim-tree.lua'
-  use 'nvim-tree/nvim-web-devicons'
-  
-  use {
-   'VonHeikemen/lsp-zero.nvim',
-   branch = 'v2.x',
-   requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      run = function()
-        pcall(vim.cmd, 'MasonUpdate')
-      end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  'nvim-tree/nvim-tree.lua',
+  'nvim-tree/nvim-web-devicons',
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-   }
- }
+  'lifepillar/vim-gruvbox8',
+  'tpope/vim-commentary',
+  'neovimhaskell/haskell-vim',
+  'haskell/stylish-haskell',
 
- use  'lifepillar/vim-gruvbox8'
- use  'tpope/vim-commentary'
- use  'neovimhaskell/haskell-vim'
- use  'haskell/stylish-haskell'
+  { 'saecki/crates.nvim', 
+    tag = 'stable',
+    config = function() 
+	require('crates').setup()
+    end,
+  },
 
- use {
-	 'saecki/crates.nvim', 
-	 tag = 'stable',
- 	 config = function() 
-		require('crates').setup()
-	 end,
- }
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
 
-end)
+})
