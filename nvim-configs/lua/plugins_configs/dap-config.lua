3bcf27a9525ee55a95f9a017bc0c6822aae21d29
fede183 <fede.183@hotmail.com>
2024-08-18 16:27:50 -0300

    submodule update

--- nvim-configs/lua/plugins_configs/dap-config.lua
+++ nvim-configs/lua/plugins_configs/dap-config.lua
@@ -11,3 +11,7 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
+
+vim.keymap.set("n", "<Leader>dt", ':DapToggleBreakpoint<CR>')
+vim.keymap.set("n", "<Leader>dx", ':DapTerminate<CR>')
+vim.keymap.set("n", "<Leader>do", ':DapStepOver<CR>')
